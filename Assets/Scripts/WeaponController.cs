861e2d6e7bd936cc296b1ec5cd8c7e62fc462152
MDohny <marek.dohnal@pslib.cz>
2019-11-04 01:05:05 +0100

    Added weapon and shooting

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public float fireballSpeed;
+
+    public Transform firePoint;
+    public GameObject fireballPrefab;
+
+    void Update()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotationZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ - 90);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootFireball();
+        }
+    }
+
+    private void ShootFireball()
+    {
+        GameObject fireball = Instantiate(fireballPrefab, firePoint.position, transform.rotation);
+        Rigidbody2D fireballRigidbody = fireball.GetComponent<Rigidbody2D>();
+        fireballRigidbody.AddForce(firePoint.up * fireballSpeed, ForceMode2D.Impulse);
+    }
+}
